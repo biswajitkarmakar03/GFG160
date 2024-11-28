31757be0774c773ce3e8bbdbf90b30b43ff68c85
Biswajit Karmakar <140104495+biswajitkarmakar03@users.noreply.github.com>
2024-11-28 23:12:32 +0530

    Day Eleven

---
+++ maximumproductsubarray.py
@@ -0,0 +1,21 @@
+class Solution:
+
+	def maxProduct(self,arr):
+		
+		n = len(arr)
+		
+		currMax = arr[0]
+		currMin = arr[0]
+		maxProd = arr[0]
+		
+		for i in range(1, n):
+		    
+		    temp = max(arr[i], arr[i] * currMax, arr[i] * currMin)
+		    
+		    currMin = min(arr[i], arr[i] * currMax, arr[i] * currMin)
+		    
+		    currMax = temp
+		    
+		    maxProd = max(maxProd, currMax)
+		    
+        return maxProd
