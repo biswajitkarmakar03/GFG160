629f255e8545089515f65a848efa77edd7a792e1
Biswajit Karmakar <140104495+biswajitkarmakar03@users.noreply.github.com>
2024-12-15 23:51:19 +0530

    Day Twenty Eight

---
+++ NumberofOccurrence.py
@@ -0,0 +1,33 @@
+def lower_bound(arr, target):
+    res = len(arr)
+    lo, hi = 0, len(arr) - 1
+    
+    while lo <= hi:
+        mid = lo + (hi - lo) // 2
+        if arr[mid] >= target:
+            res = mid
+            hi = mid - 1
+        else:
+            lo = mid + 1
+            
+    return res
+    
+def upper_bound(arr, target):
+    res = len(arr)
+    lo, hi = 0, len(arr) - 1
+    
+    while lo <= hi:
+        mid = lo + (hi - lo) // 2
+        if arr[mid] > target:
+            res = mid
+            hi = mid - 1
+        else:
+            lo = mid + 1
+            
+    return res
+
+class Solution:
+    def countFreq(self, arr, target):
+        
+        return upper_bound(arr, target) - lower_bound(arr, target)
+       
