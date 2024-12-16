3bfa591c0049fd4b2c6d882c1bef8a42667fd618
Biswajit Karmakar <140104495+biswajitkarmakar03@users.noreply.github.com>
2024-12-16 22:36:47 +0530

    Day Twenty Nine

---
+++ SortedandRotatedMinimum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMin(self, arr):
+        
+        lo, hi = 0, len(arr) - 1
+        
+        while lo < hi:
+            
+            if arr[lo] < arr[hi]:
+                return arr[lo]
+                
+            mid = (lo + hi) // 2
+            
+            if arr[mid] > arr[hi]:
+                lo = mid + 1
+                
+            else:
+                hi = mid
+                
+        return arr[lo]
