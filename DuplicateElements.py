517a3ff3802dcc5c890b355138ebb8c096f8548a
Biswajit Karmakar <140104495+biswajitkarmakar03@users.noreply.github.com>
2025-01-01 16:19:36 +0530

    Day Fourty Five

---
+++ DuplicateElements.py
@@ -0,0 +1,12 @@
+class Solution:
+    def intersectionWithDuplicates(self, a, b):
+        
+        sa = set(a)
+        res = []
+        
+        for elem in b:
+            if elem in sa:
+                res.append(elem)
+                sa.remove(elem)
+                
+        return res
