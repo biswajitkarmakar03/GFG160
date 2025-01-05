afd20b8fe41a9c49e34529637e859a15dd2c26ae
Biswajit Karmakar <140104495+biswajitkarmakar03@users.noreply.github.com>
2025-01-05 23:31:01 +0530

    Day fourty nine

---
+++ SubarrayswithSumK.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countSubarrays(self, arr, k):
+        prefixSums = {}
+        res = 0
+        currSum = 0
+        
+        for val in arr:
+            currSum += val
+            
+            if currSum == k:
+                res += 1
+            
+            if currSum - k in prefixSums:
+                res += prefixSums[currSum - k]
+                
+            prefixSums[currSum] = prefixSums.get(currSum, 0) + 1
+            
+        return res
+            
