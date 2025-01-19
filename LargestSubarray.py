e6e1d2acef50380fde5142a3b6f8a8565ffade4c
Biswajit Karmakar <140104495+biswajitkarmakar03@users.noreply.github.com>
2025-01-19 23:47:24 +0530

    Day Sixty Three

---
+++ LargestSubarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxLen(self, arr):
+        
+        mp = {}
+        
+        preSum = 0
+        res = 0
+        
+        for i in range(len(arr)):
+            preSum += -1 if arr[i] == 0 else 1
+            
+            if preSum == 0:
+                res = i + 1
+                
+            if preSum in mp:
+                res = max(res, i - mp[preSum])
+                
+            else:
+                mp[preSum] = i
+                
+        return res
